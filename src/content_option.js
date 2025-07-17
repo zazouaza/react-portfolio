2a348ef51cfde2b8595aeaadf001e948ae52b210
zazou ya kho <zazourighi@gmail.com>
2025-07-17 02:55:54 +0100

    all done

---
+++ src/content_option.js
@@ -0,0 +1,170 @@
+const logotext = "Dhiae";
+const meta = {
+    title: "Righi Dhiaeddine",
+    description: "I’m Righi Dhiaeddine data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Righi Dhiaeddine",
+    animated: {
+        first: "coding entuhsiast ",
+        second: "also an Ai ENTHUSIAST aka 'vibe coder'",
+        third: "i do both frontend and backend",
+    },
+    description: "I'm a full stack developer creating user-friendly and efficient web applications. I'm also an AI enthusiast . I'm currently in Algeria building webapps trying to fill up my resume .",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "a bit about my self",
+    aboutme: " I'm a fresh Masters graduate trying to  learn  and challenge my self more and get into the tech field ",
+};
+const worktimeline = [{
+        jobtitle: "an web app for renting cars",
+        where: "akri l'auto bl5ef",
+        date: "2024",
+    },
+    {
+        jobtitle: "secure web application for a medical clinic ",
+        where: "benchrif clinique",
+        date: "2025",
+    },
+    
+];
+
+const skills = [{
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "php/laravel",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "MYSQL",
+        value: 60,
+    },
+    {
+        name: "Node.js",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "I can design modern UIs depending on your taste and needs.",
+    },
+    {
+        title: "Backend",
+        description: "I can set up your whole database starting from the schema.",
+    },
+    {
+        title: "Landing pages ",
+        description: "I can create a modern landing page for your business",
+    },
+    {
+        title: "ecommerce webapps ",
+        description: "I can create an ecommerce website integrate payment and also a modern admin dashboared that will make your business flow much easier",
+    },
+    {
+        title: "Full stack webapps",
+        description: "I can do all the work on a full stack web app both the frontend and backend ",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "zazourighi@gmail.com",
+    YOUR_FONE: "+213 672399411",
+    description: "i'm availlable on whats app and i try to respond as quick as possible to the emails  ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/zazouaza",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/righi-dhiaeddine-383b46317/",
+    twitter: "https://x.com/zazouchilling00",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
